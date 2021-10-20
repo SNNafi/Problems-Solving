a4cf4842c719ff2f799ac111abc0abc854c76cf8
Shahriar Nasim Nafi <shahriarnasim.nafi@gmail.com>
2021-10-20 22:39:46 +0600

    Done 16bitswonly DMOJ

---
+++ DMOJ/16bitswonly/16bitswonly.cpp
@@ -0,0 +1,37 @@
+//
+// Created by Shahriar Nasim Nafi on 20/10/21.
+//
+
+#include <iostream>
+
+
+using namespace std;
+
+struct BIT {
+    unsigned long long a;
+    unsigned long long b;
+    unsigned long long p;
+
+    string check() {
+        if (p == a * b) {
+            return "POSSIBLE DOUBLE SIGMA";
+        }
+        return "16 BIT S/W ONLY";
+    }
+};
+
+int main() {
+
+    BIT bits[1000];
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        BIT bit;
+        cin >> bit.a >> bit.b >> bit.p;
+        bits[i] = bit;
+    }
+    for (int i = 0; i < n; i++) {
+        BIT bit = bits[i];
+        cout << bit.check() << endl;
+    }
+}
