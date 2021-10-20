7c6e4876c75ae981b462fa39765d35a153f59055
Shahriar Nasim Nafi <shahriarnasim.nafi@gmail.com>
2021-10-20 22:24:26 +0600

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/AddTwoNumbers.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "AddTwoNumbers"
+  "AddTwoNumbers.pdb"
+  "CMakeFiles/AddTwoNumbers.dir/LeetCode/AddTwoNumbers/AddTwoNumbers.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AddTwoNumbers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
